5c712627df9618b78fe395c656b8faa8d41655ac
ymj <2513667783@qq.com>
2021-07-22 12:55:53 +0800

    递归方法实现归并排序

---
+++ src/com/test/str/test01/class08/Code02_MergeSort.java
@@ -0,0 +1,51 @@
+package com.test.str.test01.class08;
+
+/**
+ * @Classname Code02_MergeSort
+ * @Description TODO
+ * @Date 2021/7/21 12:59
+ * @Created by yemingjie
+ */
+public class Code02_MergeSort {
+    // 递归方法实现
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    // arr[L...R]范围上，请让这个范围上的数，有序！
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+        // int mid = (L + R) / 2
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        // 要么p1越界，要么p2越界
+        // 不可能出现：共同越界
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
+}
