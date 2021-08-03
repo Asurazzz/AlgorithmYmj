11e7d7a5badbae4b0793efd5facba566def478ff
ymj <2513667783@qq.com>
2021-08-03 13:16:14 +0800

    插入排序

---
+++ src/com/test/str/test02/class01/Code03_InsertionSort.java
@@ -0,0 +1,26 @@
+package com.test.str.test02.class01;
+
+/**
+ * @Classname Code03_InsertionSort
+ * @Description TODO
+ * @Date 2021/8/3 13:01
+ * @Created by yemingjie
+ */
+public class Code03_InsertionSort {
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--) {
+                swap(arr, j, j+1);
+            }
+        }
+    }
+    // i和j是一个位置的话，会出错
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+}
