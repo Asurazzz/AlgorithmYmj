6bf8ac96b0076d9eb958cbd4dae68a19f94a8805
develop3 <2513667783@qq.com>
2021-11-10 17:58:01 +0800

    请实现一个函数，把字符串 s 中的每个空格替换成"%20"。

---
+++ src/com/test/str/test00/Solution.java
@@ -0,0 +1,28 @@
+package com.test.str.test00;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ * 0 <= s 的长度 <= 10000
+ */
+public class Solution {
+
+    public String replaceSpace(String s) {
+        if (s.length() < 0 || s.length() > 10000) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+
+    }
+
+}
