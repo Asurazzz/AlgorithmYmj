bfd6bd37432912d9d2f8a1010b05f6bb74528d55
develop3 <2513667783@qq.com>
2021-12-10 18:12:12 +0800

    .

--- src/com/test/str/test01/class18/Code01_RobotWalk.java
+++ src/com/test/str/test01/class18/Code01_RobotWalk.java
@@ -2,6 +2,8 @@ package com.test.str.test01.class18;
 
 /**
  * 机器人走路
+ *
+ * @author admin
  */
 public class Code01_RobotWalk {
 
@@ -43,8 +45,59 @@ public class Code01_RobotWalk {
 
     }
 
+
+    public static int ways2(int N, int start, int aim, int K) {
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        /**
+         * dp就是缓存表
+         * dp[cur][rest] == -1   ---> process1(cur, rest)之前没算过！
+         * dp[cur][rest] != -1   ---> process1(cur, rest)之前算过！算完的返回值放在了dp[cur][rest]中
+         * N+1 * K+1
+         */
+        return process2(start, K, aim, N, dp);
+    }
+
+    /**
+     * cur 的范围：1 ~ N
+     * rest的范围：0 ~ K
+     * 自顶向下的动态规划
+     * 动态规划： 所有的重复过程只触发了一次
+     * @param cur
+     * @param rest
+     * @param aim
+     * @param N
+     * @return
+     */
+    public static int process2(int cur, int rest, int aim, int N, int[][] dp) {
+        if (dp[cur][rest] != -1) {
+            // 表示之前来过
+            return dp[cur][rest];
+        }
+        // 之前没有算过！
+        int ans = 0;
+        if (rest == 0) {
+            ans = cur == aim ? 1 : 0;
+        } else if (cur == 1) {
+            ans = process2(2, rest - 1, aim, N, dp);
+        } else if (cur == N) {
+            ans = process2(N - 1, rest - 1, aim, N, dp);
+        } else {
+            ans = process2(cur - 1, rest - 1, aim, N, dp) + process2(cur + 1, rest - 1, aim, N, dp);
+        }
+        dp[cur][rest] = ans;
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(ways1(4, 2, 4, 4));
+        System.out.println(ways2(4, 2, 4, 4));
+        System.out.println(ways2(5, 2, 4, 6));
     }
 
 }
