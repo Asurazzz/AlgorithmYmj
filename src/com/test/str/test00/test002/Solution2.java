e4c8a31424fe2668b5dfb7a489d9e691c8b3fb0d
develop3 <2513667783@qq.com>
2021-12-10 11:53:13 +0800

    .

---
+++ src/com/test/str/test00/test002/Solution2.java
@@ -0,0 +1,18 @@
+package com.test.str.test00.test002;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int i = maxSubArray(nums);
+        System.out.println(i);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+}
